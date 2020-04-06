d644640ab71f0bff814505a7100862ca94ca85de
Valgulious <dmitryfedorenkof@gmail.com>
2020-04-06 18:11:00 +1000

    done?

--- ElCur.py
+++ ElCur.py
@@ -16,15 +16,20 @@ class ElCur:
 
 	def __init__(self, _p, _A, _B, _G, _O):
 		if not ((4 * _A ** 3 + 27 * _B ** 2) % _p):
-			print("низя")
+			print("Невозможно создать эллиптическую кривую с заданными параметрами A и B")
 			return
 
 		self.p = _p
 		self.A = _A
 		self.B = _B
 		self.O = _O
-		self.G = _G
-		# self.points = self.get_all_points()
+		if self.point_belongs(_G[0], _G[1]):
+			self.G = _G
+			self.points = self.get_all_points()
+		else:
+			print("Точка G не принадлежит заданной эллиптической кривой")
+			return
+
 
 	def add_points(self, _x1, _x2, _y1, _y2):
 		if (_x1 == _x2) and (_y1 == (-_y2) % self.p):
@@ -44,6 +49,7 @@ class ElCur:
 		points = [self.G]
 		x1 = x2 = self.G[0]
 		y1 = y2 = self.G[1]
+		count = 1
 
 		while flag:
 			new_point = self.add_points(x1, x2, y1, y2)
@@ -58,15 +64,20 @@ class ElCur:
 				y2 = new_point[1]
 				print(new_point)
 
+			count += 1
+
+		print("Количство точек = " + str(count))
+
 		return points
 
 	def get_point(self, _n):
-		return self.mul_point(_n, self.G)
+		return self.points[_n - 1]
+		# return self.mul_point(_n, self.G)
 
 	def mul_point(self, _n, _point):
 		x1 = x2 = _point[0]
 		y1 = y2 = _point[1]
-		new_point = (0, 0)
+		new_point = _point
 
 		for i in range(1, _n):
 			new_point = self.add_points(x1, x2, y1, y2)
@@ -83,19 +94,3 @@ class ElCur:
 			return True
 		else:
 			return False
-
-
-el = ElCur(31991, 31988, 1000, (0, 5585), (0, 0))
-# print(el.get_point(5103))
-# print(el.get_point(523))
-# print(el.mul_point(523, (12507, 2027)))
-# el.print_points()
-# print(el.get_point(0, 0, 5585, 5585))
-# print(el.point_belongs(8.201019196290101, -5608.485604480835))
-# print(el.point_belongs(0, 26406))
-# print(el.point_belongs(0, 0))
-# print(el.add_points(8, 0, 19435, 0))
-# print((0, 0) in el.points)
-# for p in el.points:
-# 	if el.point_belongs(p[0], p[1]):
-# 		print(p)
--- ElGamal.py
+++ ElGamal.py
@@ -1,28 +1,67 @@
-from random import random
-# from ElCur import ElCur
+from random import randint
 import ElCur
 
 
 class User:
-	def __init__(self, _elcur):
+	def __init__(self, _id, _name, _elcur):
+		self.id = _id
+		self.name = _name
 		self.elcur = _elcur
-		self._secret_key = 5103
+		self._secret_key = randint(1, len(self.elcur.points) - 1)
 		self.public_key = self.elcur.get_point(self._secret_key)
 
 	def encrypt(self, _message, _public_key):
-		k = 523
-		r = self.elcur.get_point(k)
-		p = self.elcur.mul_point(k, _public_key)
-		encrypt_message = (_message * p[0]) % self.elcur.p
-		return r, encrypt_message
+		k = randint(1, len(self.elcur.points) - 1)
+		R = self.elcur.get_point(k)
+		P = self.elcur.mul_point(k, _public_key)
+		encrypt_message = (_message * P[0]) % self.elcur.p
+		return R, encrypt_message
 
-	def decrypt(self, _r, _encrypt_message):
-		q = self.elcur.mul_point(self._secret_key, _r)
-		message = (_encrypt_message * ElCur.mulinv(q[0], self.elcur.p)) % self.elcur.p
+	def decrypt(self, _encrypt_message):
+		Q = self.elcur.mul_point(self._secret_key, _encrypt_message[0])
+		message = (_encrypt_message[1] * ElCur.mulinv(Q[0], self.elcur.p)) % self.elcur.p
 		return message
 
 
-el = ElCur.ElCur(31991, 31988, 1000, (0, 5585), (0, 0))
-user = User(el)
-print(user.encrypt(10000, (12507, 2027)))
-print(user.decrypt((9767, 11500), 11685))
+class ElGamal:
+	def __init__(self, _p, _A, _B, _G, _O):
+		self.elcur = ElCur.ElCur(_p, _A, _B, _G, _O)
+		self.users = []
+
+	def add_user(self, _id, _name):
+		for user in self.users:
+			if _id == user.id:
+				print("Пользователь с id: " + str(_id) + " уже существует!")
+				return False
+		self.users.append(User(_id, _name, self.elcur))
+		print("Пользователь с id: " + str(_id) + " и именем: " + str(_name) + " создан!")
+		return True
+
+	def remove_user(self, _id):
+		for user in self.users:
+			if _id == user.id:
+				self.users.remove(user)
+				print("Плользователь с id: " + str(_id) + " удален!")
+				return True
+
+		print("Пользователя с id: " + str(_id) + " не существует!")
+		return False
+
+	def	send_massage(self, _message, _user_from, _user_to):
+		print("Сообщение, которое надо передать: " + str(_message))
+		encrypt_message = _user_from.encrypt(_message, _user_to.public_key)
+		print("Зашифрованное сообщение: " + str(encrypt_message))
+		print("Пользователь: " + _user_from.name + " посылает сообщение пользователю: " + _user_to.name)
+		decrypt_message = _user_to.decrypt(encrypt_message)
+		print("Расшифрованное сообщение: " + str(decrypt_message))
+
+
+p = 31991
+A = 31988
+B = 1000
+G = (0, 5585)
+O = (0, 0)
+elgamal = ElGamal(p, A, B, G, O)
+elgamal.add_user(0, "Alice")
+elgamal.add_user(1, "Bob")
+elgamal.send_massage(10000, elgamal.users[0], elgamal.users[1])
